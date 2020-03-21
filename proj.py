07aea5ef5cfc1c3216cae0e0929609e8935ce5e4
sparshamit2712 <41731634+sparshamit2712@users.noreply.github.com>
2020-03-21 12:09:18 +0530

    Create proj.py

---
+++ proj.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Chrome(executable_path=r"C:\Users\cools\Desktop\chromedriver_win32\chromedriver.exe")
+from time import sleep
+
+#Login
+br = driver.get('http://54.235.233.16/')
+sr = driver.find_element_by_id('logemail')
+sr.send_keys("amazonaniruddha123@gmail.com")
+pk = driver.find_element_by_id('logpsk')
+pk.send_keys("University@99")
+driver.find_element_by_name('login').click()
+
+#Register
+#driver.find_element_by_id('signfirstname').send_keys("Sparsh")
+#driver.find_element_by_id('signsurname').send_keys("Amit")
+#driver.find_element_by_id('signemail').send_keys("sparshamit2717@gmail.com")
+#driver.find_element_by_id('signpassword').send_keys("sparshbhai")
+#driver.find_element_by_id('signday').send_keys("27")
+#driver.find_element_by_id('signmonth').send_keys("Dec")
+#driver.find_element_by_id('signyear').send_keys("1998")
+#gen = "Male"
+#if(gen=="Male"):
+#    driver.find_element_by_id('signgender1').click()
+#elif(gen=="Female"):
+#    driver.find_element_by_id('signgender2').click()
+#else:
+#    driver.find_element_by_id('signgender3').click()
+#driver.find_element_by_name('register').click()
